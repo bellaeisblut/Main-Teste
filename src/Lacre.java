57604c57e59cd860397b1b3ba9680da2f1f59fa0
bellaeisblut <bellafalete@gmail.com>
2026-08-24 10:39:46 -0300

    Finalizei o teste agr

--- src/Lacre.java
+++ src/Lacre.java
@@ -1,3 +1,14 @@
+
+import java.util.Scanner;
+
 public class Lacre {
+
+    public static void main(String[] args) {
+        
+        try (Scanner leia = new Scanner(System.in)) {
+
+            
+        }
+    }
 
 }
